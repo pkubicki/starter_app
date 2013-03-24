879e963cb5f11fbd2dc5119735e88f5d1fb4bbdd
Paweł Kubicki <pawel.kubicki@gmail.com>
2013-03-24 09:28:47 +0100

    missing jquery plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.datepicker
+//= require jquery.ui.slider
 //= require jquery-ui-timepicker-addon
 
 //= require bootstrap-transition
