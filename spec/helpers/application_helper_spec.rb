4c0d486b79bc65f0a34abc6e883af8bcd5e1de1b
Paweł Kubicki <pawel.kubicki@gmail.com>
2013-03-28 20:25:05 +0100

    application helper specs

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-  include Capybara::DSL
 
   describe "#flash_messages" do
     before do
@@ -46,4 +45,124 @@ describe ApplicationHelper do
   describe "#currency" do
     it { expect(helper.currency(1_000.25)).to eq('1 000,25') }
   end
+
+  describe "#link_to_with_icon" do
+    subject do 
+      Capybara.string(
+	helper.link_to_with_icon(
+	  'Lorem ipsum', 
+	  'icon-name', 
+	  'url',
+	  { class: 'class-name' }
+	)
+      )
+    end
+
+    it { should have_selector('a.class-name i.icon-name') }     
+    it { should have_content('Lorem ipsum') }     
+  end
+
+  describe "#button_with_icon" do
+    subject do 
+      Capybara.string(
+	helper.button_with_icon(
+	  'Lorem ipsum', 
+	  'icon-name', 
+	  { class: 'class-name' }
+	)
+      )
+    end
+
+    it { should have_selector('button.class-name i.icon-name') }     
+    it { should have_content('Lorem ipsum') }     
+  end
+ 
+  describe "#nav_link_to" do
+    context "when not allowed to" do
+      subject do 
+        helper.nav_link_to(
+          'Lorem ipsum', 
+          '/', 
+          { class: 'class-name' }
+        )
+      end 
+      before { helper.stub(:allowed_to?) { false } }
+      it { should be_nil }     
+    end
+    
+    context "when allowed to" do
+      subject do 
+        Capybara.string(
+          helper.nav_link_to(
+            'Lorem ipsum', 
+            '/', 
+            { class: 'class-name' }
+          )
+        )
+      end
+      before { helper.stub(:allowed_to?) { true } }
+      it { should have_selector('li a.class-name') }     
+      it { should have_content('Lorem ipsum') }     
+    end 
+  end
+ 
+  describe "#nav_link_to_with_icon" do
+    context "when not allowed to" do
+      subject do 
+        helper.nav_link_to_with_icon(
+          'Lorem ipsum',
+	  'icon-name', 
+          '/', 
+          { class: 'class-name' }
+        )
+      end 
+      before { helper.stub(:allowed_to?) { false } }
+      it { should be_nil }     
+    end
+    
+    context "when allowed to" do
+      subject do 
+        Capybara.string(
+          helper.nav_link_to_with_icon(
+            'Lorem ipsum', 
+	    'icon-name', 
+            '/', 
+            { class: 'class-name' }
+          )
+        )
+      end
+      before { helper.stub(:allowed_to?) { true } }
+      it { should have_selector('li a.class-name i.icon-name') }     
+      it { should have_content('Lorem ipsum') }     
+    end 
+  end
+ 
+  describe "#nav_dropdown" do
+    subject { Capybara.string(helper.nav_dropdown('Lorem ipsum') {}) }
+
+    it { should have_selector('li.dropdown a.dropdown-toggle b.caret') } 
+    it { should have_selector('li.dropdown ul.dropdown-menu') } 
+  end 
+ 
+  describe "#ldt" do
+    context "when nil given" do
+      it { expect(helper.ldt(nil)).to be_nil }
+    end
+
+    context "when nil given" do
+      it { expect(helper.ldt("2013-01-01 12:00:00".to_datetime)).to eq("2013-01-01 12:00:00") }
+    end
+  end
+
+  describe "#ldt" do
+    context "when 'q' params contain datetime value" do
+      before { helper.stub(:params).and_return({ q: { begin_at: "2013-01-01 12:00:00".to_datetime } }) }
+      it { expect(helper.ldtqp(:begin_at)).to eq("2013-01-01 12:00:00") }
+    end
+
+    context "when 'q' params don't exist" do
+      before { helper.stub(:params).and_return({}) }
+      it { expect(helper.ldtqp(:begin_at)).to be_nil }
+    end 
+  end   
 end
